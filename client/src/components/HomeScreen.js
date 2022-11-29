76193582ce36b234b3c7f79069160c3e4da3fed9
YonglinMai <43829014+YonglinMai@users.noreply.github.com>
2022-11-28 19:25:24 -0500

    Split the homeScreen into list and songPlayer

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { GlobalStoreContext } from '../store'
 import ListCard from './ListCard.js'
 import MUIDeleteModal from './MUIDeleteModal'
@@ -7,6 +7,17 @@ import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab'
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box'
+import HomeIcon from '@mui/icons-material/Home';
+import Groups2Icon from '@mui/icons-material/Groups2';
+import PersonIcon from '@mui/icons-material/Person';
+import SortIcon from '@mui/icons-material/Sort';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import SearchIcon from '@mui/icons-material/Search';
 /*
     This React component lists all the top5 lists in the UI.
     
@@ -14,6 +25,8 @@ import Typography from '@mui/material/Typography'
 */
 const HomeScreen = () => {
     const { store } = useContext(GlobalStoreContext);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const isMenuOpen = Boolean(anchorEl);
 
     useEffect(() => {
         store.loadIdNamePairs();
@@ -22,6 +35,45 @@ const HomeScreen = () => {
     function handleCreateNewList() {
         store.createNewList();
     }
+
+    const handleMenuOpen = (event) => {
+        console.log("menu")
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const styleForButton = {
+        width: '45px',
+        height: '45px',
+        padding: "10",
+      };
+
+      const sortMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem>Name (A - Z) </MenuItem>
+            <MenuItem>Published Date (Newest) </MenuItem>
+            <MenuItem>Listens (High to Low) </MenuItem>
+            <MenuItem>Likes (High to Low)</MenuItem>
+            <MenuItem>Dislikes (High to Low)</MenuItem>
+        </Menu>
+      );
+
     let listCard = "";
     if (store) {
         listCard = 
@@ -50,11 +102,48 @@ const HomeScreen = () => {
             </Fab>
                 <Typography variant="h2">Your Lists</Typography>
             </div>
-            <div id="list-selector-list">
-                {
-                    listCard
-                }
-                <MUIDeleteModal />
+            <div id="home-Screen">
+                <div id="tool-bar">
+                    <Box id="toolbar-button" sx={{transform:"translate(0%, 20%)"}}>
+                        <HomeIcon style={styleForButton}></HomeIcon>
+                        <Groups2Icon style={styleForButton}></Groups2Icon>
+                        <PersonIcon style={styleForButton}></PersonIcon>
+                    </Box>
+                    <Box component="form" sx={{transform:"translate(90%, 0%)"}}>
+                        <TextField
+                            margin="normal"
+                            size="small"
+                            id="search-textfield"
+                            label="search"
+                            name="search"
+                            autoComplete="search"
+                            sx={{width: '75%', height: '10%', left: '0%'}}
+                            autoFocus
+                        />
+                        <SearchIcon style = {styleForButton} sx={{transform:"translate(0%, 10%)"}}></SearchIcon>
+                    </Box> 
+                    <Box sx ={{transform:"translate(380%, -20%)"}}>
+                        <Typography  variant="string">Sort By </Typography>
+                        <SortIcon 
+                            sx ={{transform:"translate(0%, 35%)"}} 
+                            style={styleForButton} 
+                            onClick={handleMenuOpen}>
+                        </SortIcon>
+                        {sortMenu}
+                    </Box>
+                </div>
+                <div></div>
+                <div id="user-screen">
+                    <div id="list-selector-list">
+                        {
+                        listCard
+                        }
+                        <MUIDeleteModal />
+                    </div>
+                    <div id="song-player-screen">
+                        testtesttest
+                    </div>
+                </div>
             </div>
         </div>)
 }
